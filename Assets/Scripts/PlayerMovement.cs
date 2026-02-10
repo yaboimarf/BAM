ccc49a512f71dd00a53bc650272410cd70f3c26c
yaboimarf <mfolst2006@gmail.com>
2026-02-10 11:37:57 +0100

    PlayerMovement 0.2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+    public Vector3 moveDir;
+    public float moveSpeed;
+    public Vector3 bodyRotate;
+    public Vector3 camRotate;
+    public float rotateSpeed;
+    public Transform cam;
+    public Rigidbody rb;
+    public float jumpStrength;
     // Update is called once per frame
     void Update()
     {
-        
+        BodyMovement();
+        CamMovement();
+        Jump();
+    }
+    private void BodyMovement()
+    {
+        moveDir.x = Input.GetAxis("Horizontal");
+        moveDir.z = Input.GetAxis("Vertical");
+
+        rb.AddRelativeForce(moveSpeed * Time.deltaTime * moveDir, ForceMode.Impulse);
+        //transform.Translate(moveDir * Time.deltaTime * moveSpeed);
+    }
+    private void CamMovement()
+    {
+        bodyRotate.y = Input.GetAxis("Mouse X");
+        camRotate.x = -Input.GetAxis("Mouse Y");
+
+        transform.Rotate(bodyRotate * Time.deltaTime * rotateSpeed);
+        cam.Rotate(camRotate * Time.deltaTime * rotateSpeed);
+    }
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * Time.deltaTime * jumpStrength, ForceMode.Impulse);
+        }
     }
 }
