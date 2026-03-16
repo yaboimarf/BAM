ed32ef5564edb68a55a65c5f6346df00fad67eb4
yaboimarf <mfolst2006@gmail.com>
2026-03-16 16:05:19 +0100

    Player Movement 0.7
    im going to strangel the person responsible for transform.rotation and the way it works

---
+++ Assets/Scripts/PlayerMovement2.cs
@@ -0,0 +1,189 @@
+using JetBrains.Annotations;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerMovement2 : MonoBehaviour
+{
+    public Vector3 moveDir;
+    public float moveSpeed;
+    public float notGroundedPenalty;
+    public float boostSpeed;
+    public Vector3 bodyRotate;
+    public Vector3 camRotate;
+    public float rotateSpeed;
+    public Transform cam;
+    public Rigidbody rb;
+    public float jumpStrength;
+    public float gravity;
+
+    public float groundDistance;
+    public RaycastHit groundedHit;
+    public bool grounded;
+
+    public float wallDistance;
+    public RaycastHit walledHit;
+    public bool walled;
+
+    private int doubleJumpsRemaining;
+    public int doubleJumps;
+    private int dashesRemaining;
+    public int dashes;
+
+    public float batteryRemaining;
+    public float batteryMax;
+    public float batteryMin;
+    public float chargeRate;
+    public float dashCost;
+    public float jumpCost;
+
+    public float maxDampening;
+    public float minDampening;
+
+    public float minClamp;
+    public float maxClamp;
+    public float valueX;
+    public float valueX2;
+    // Update is called once per frame
+    void Update()
+    {
+        if (grounded == true)
+        {
+            if (batteryRemaining < batteryMax)
+            {
+                batteryRemaining += chargeRate * Time.deltaTime;
+            }
+        }
+        if(batteryRemaining < batteryMin)
+        {
+            batteryRemaining = batteryMin;
+        }
+
+        BodyMovement();
+        Jump();
+        WallCling();
+
+        //xValue = camRotate.x;
+
+        //camRotate = Mathf.Clamp(xValue, minClamp, maxClamp);
+        //if (Physics.Raycast(transform.position, -transform.up, out groundedHit, groundDistance))
+        //{
+        //    grounded = true;
+        //    doubleJumpsRemaining = doubleJumps;
+        //    dashesRemaining = dashes;
+        //}
+        //else
+        //{
+        //    grounded = false;
+        //}
+    }
+    private void BodyMovement()
+    {
+        //body movement
+        moveDir.x = Input.GetAxis("Horizontal");
+        moveDir.z = Input.GetAxis("Vertical");
+
+        if (grounded == true)
+        {
+            rb.AddRelativeForce(moveSpeed * Time.deltaTime * moveDir, ForceMode.Impulse);
+        }
+        else
+        {
+            rb.AddRelativeForce(moveSpeed * notGroundedPenalty * Time.deltaTime * moveDir, ForceMode.Impulse);
+        }
+
+        // cam movement
+
+        bodyRotate.y = Input.GetAxis("Mouse X");
+        camRotate.x = -Input.GetAxis("Mouse Y");
+        
+        transform.Rotate(bodyRotate * Time.deltaTime * rotateSpeed);
+        cam.Rotate(camRotate * Time.deltaTime * rotateSpeed);
+
+        //valueX = cam.rotation.x;
+        //valueX = Mathf.Clamp(valueX, minClamp, maxClamp);
+
+
+
+        // dashes
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (batteryRemaining >= dashCost)
+            {
+                if (grounded == true)
+                {
+                    rb.AddForce(transform.forward * boostSpeed * 5, ForceMode.Impulse);
+                }
+                else
+                {
+                    rb.AddForce(transform.forward * boostSpeed, ForceMode.Impulse);
+                }
+                batteryRemaining -= dashCost;
+            }
+        }
+    }
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (batteryRemaining >= jumpCost)
+            {
+                rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
+                batteryRemaining -= jumpCost;
+            }
+        }
+
+        // checks for sky vs ground linear dampening
+        if (grounded == true)
+        {
+            rb.linearDamping = maxDampening;
+        }
+        else
+        {
+            rb.linearDamping = minDampening;
+        }
+
+        // checks if player is allowed to jump
+        if (Physics.Raycast(transform.position, -transform.up, out groundedHit, groundDistance))
+        {
+            grounded = true;
+            //doubleJumpsRemaining = doubleJumps;
+            //dashesRemaining = dashes;
+        }
+        else
+        {
+            grounded = false;
+        }
+    }
+    private void WallCling()
+    {
+        if (grounded == false)
+        {
+            if (Physics.Raycast(transform.position, transform.right, out walledHit, wallDistance))
+            {
+                rb.useGravity = false;
+                rb.AddForce(1 * Time.deltaTime * transform.right, ForceMode.Impulse);
+                rb.AddRelativeForce(2 * Time.deltaTime * -transform.up, ForceMode.Impulse);
+                rb.AddForce(10 * Time.deltaTime * transform.forward, ForceMode.Impulse);
+                //walled = true;
+                Debug.Log("ik werk");
+            }
+
+            if (Physics.Raycast(transform.position, -transform.right, out walledHit, wallDistance))
+            {
+                rb.useGravity = false;
+                rb.AddForce(1 * Time.deltaTime * -transform.right, ForceMode.Impulse);
+                rb.AddRelativeForce(2 * Time.deltaTime * -transform.up, ForceMode.Impulse);
+                rb.AddForce(10 * Time.deltaTime * transform.forward, ForceMode.Impulse);
+                //walled = true;
+            }
+
+            if (!Physics.Raycast(transform.position, -transform.right, out walledHit, wallDistance))
+            {
+                if (!Physics.Raycast(transform.position, transform.right, out walledHit, wallDistance))
+                {
+                    rb.useGravity = true;
+                }
+            }
+        }
+    }
+}
